e0b888b65fc83d78dcbd0d0d9afff34334131faf
elregalo <giftmoobi@gmail.com>
2022-07-05 20:48:09 +0200

    added starter files

--- index.js
+++ index.js
@@ -1,43 +1,19 @@
-const smpp = require("smpp");
-const dotenv = require("dotenv");
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const session = new smpp.Session({
-  host: process.env.SMPP_HOST,
-  port: process.env.SMPP_PORT,
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-// bind transceiver
-let isConnected = false;
-session.on("connect", () => {
-  isConnected = true;
-
-  session.bind_transceiver(
-    {
-      system_id: process.env.SYSTEM_ID,
-      password: process.env.PASSWORD,
-      interface_version: 1,
-      system_type: process.env.SYSTEM_TYPE,
-      address_range: process.env.SYSTEM_TYPE,
-      addr_ton: 1,
-      addr_npi: 1,
-    },
-    (pdu) => {
-      if (pdu.command_status == 0) {
-        console.log("Bind Transceiver Success");
-      }
-    }
-  );
-});
-
-session.close("close", () => {
-  if (isConnected) {
-    session.connect();
-  }
-});
-
-session.on("error", (err) => {
-  console.log(err);
-  isConnected = false;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
